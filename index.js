a4184f41faf67ca92538c6b1e2a07a5ffd86c265
Jonny <62953110+Jomyy@users.noreply.github.com>
2022-04-02 21:07:57 +0200

    frfsjjnkfjnksf

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
-const rp = require('request-promise');
+const rp = require("request-promise");
+const cheerio = require("cheerio");
+const cheerioTableparser = require("cheerio-tableparser");
+const fs = require("fs");
+
 const url = "https://infoscreen.friedensschule.de";
 (function loop() {
-    setTimeout(function () {
-      // execute script
-      rp(url)
-  .then(function(html){
-    var cheerio = require('cheerio'),
-    cheerioTableparser = require('cheerio-tableparser');
+  setTimeout(function () {
+    // execute script
+    rp(url)
+      .then(function (html) {
+        $ = cheerio.load(html);
 
-    $ = cheerio.load(html);
+        cheerioTableparser($);
+        //#region load food
 
-    cheerioTableparser($);
-    //#region load food
-    
-    var data = $(".ce-table").parsetable();
-    
-    data.shift();
-    
-    var fs = require('fs');
-    var stringdat = JSON.stringify(data);
-    
-    
-    fs.writeFile("fplan.json",stringdat.replace(/\\n/g, '').replace(/\\n/g, '').replace(/\\t/g, '').replace(/\\t/g, ''), function(err, result) {
-        
-    });
-    //#endregion
+        var data = $(".ce-table").parsetable();
 
-    //#region load vplan
-    let vplan = $(".col_vplan")
-    let vplansarr = vplan.find(".tx-friedensschule")
-    let stundenfirst = []
-    let stundensecond = []
-    vplansarr.map((j,vplans) => {
-       // put every tr in a json array 
-        let vplanarr = $(vplans).find("tr")
-            vplanarr.map((i,vplan) => {
-                let vplanobj = {}
-                let vplanarr = $(vplan).find("td")
-                vplanarr.map((i,vplan) => {
-                    vplanobj[i] = $(vplan).text()
-                })
-                if(j == 0){
-                    stundenfirst.push(vplanobj);
-                }
-                if(j == 1){
-                    stundensecond.push(vplanobj);
-                }
-                
+        data.shift();
+
+        var stringdat = JSON.stringify(data);
+
+        fs.writeFile("fplan.json", stringdat.replace(/\\n/g, "").replace(/\\n/g, "").replace(/\\t/g, "").replace(/\\t/g, ""), function (err, result) {});
+        //#endregion
+
+        //#region load vplan
+        let vplan = $(".col_vplan");
+        let vplansarr = vplan.find(".tx-friedensschule");
+        let stundenfirst = [];
+        let stundensecond = [];
+        vplansarr.map((j, vplans) => {
+          // put every tr in a json array
+          let vplanarr = $(vplans).find("tr");
+          vplanarr.map((i, vplan) => {
+            let vplanobj = {};
+            let vplanarr = $(vplan).find("td");
+            vplanarr.map((i, vplan) => {
+              if (i != 0) {
+                vplanobj[i - 1] = $(vplan).text();
+              }
+            });
+            if (j == 0) {
+              if (vplanobj[1] != undefined) {
+                stundenfirst.push(vplanobj);
+              }
             }
-        )
-        
-        
-    }
-    )
-    console.log(stundenfirst.length)
-    stundenfirst.shift();
-    stundensecond.shift();
-    console.log(stundensecond.length)
-    fs.writeFile("vplan.json", JSON.stringify(stundenfirst), function(err, result) {
-        if(err) console.log('error', err);
-    });
-    fs.writeFile("vplannext.json", JSON.stringify(stundensecond), function(err, result) {
-        if(err) console.log('error', err);
-    });
-  })
-  .catch(function(err){
-    //handle error
-  });
-      loop()
-    }, 600000); //9000 = 9000ms = 9s 600000
-  }());
+            if (j == 1) {
+              if (vplanobj[1] != undefined) {
+                stundenfirst.push(vplanobj);
+              }
+              stundensecond.push(vplanobj);
+            }
+          });
+          stundenfirst.shift();
+          stundensecond.shift();
+        });
+        console.log(stundenfirst.length);
+        stundenfirst.shift();
+        stundensecond.shift();
+        console.log(stundensecond.length);
+        fs.writeFile("vplan.json", JSON.stringify(stundenfirst), function (err, result) {
+          if (err) console.log("error", err);
+        });
+        fs.writeFile("vplannext.json", JSON.stringify(stundensecond), function (err, result) {
+          if (err) console.log("error", err);
+        });
+      })
+      .catch(function (err) {
+        //handle error
+      });
+    loop();
+  }, 600000); //9000 = 9000ms = 9s 600000
+})();
